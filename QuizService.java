fac8f268a93dc98c76f9c194a5d6662e1a9307ce
shweta <shweta3407t@gmail.com>
2026-06-13 22:49:10 +0530

    combine similar method in one

--- QuizService.java
+++ QuizService.java
@@ -1,75 +1,162 @@
 package service;
 
-import model.Questions;
-import java.io.*;
+ import java.io.*;
+  import java.io.BufferedWriter;
+ import java.io.FileWriter;
+ import java.io.IOException;
 import java.util.*;
 
 public class QuizService {
-    static Questions javaQ;
-    static Questions scienceQ;
-    static Questions gkQ;
+    static ArrayList<String[]> javaQuestionList = new ArrayList<>();
+    static ArrayList<String[]> scienceQuestionList = new ArrayList<>();
+    static ArrayList<String[]> gkQuestionList = new ArrayList<>();
 
-    static  String javaAns;
-    static String sAns;
-    static String gkAns;
+    public  static  void  takeQuizz(Scanner sc ){
 
-//    static ArrayList<ArrayList<String[]>> allQuestionList = new ArrayList<>();
-    static ArrayList<String[]> javaList = new ArrayList<>();
-    static ArrayList<String[]> scienceList = new ArrayList<>();
-    static ArrayList<String[]> gkList = new ArrayList<>();
+        boolean isRunning=true;
+        while( isRunning){
 
-    public static void manageQuestion( Scanner sc ,HashMap<String, Questions> quesMap) {
+            System.out.println("""
+                    CATEGORY :
+                    1. : JAVA
+                    2. : SCIENCE
+                    3. : GENERAL KNOWLEDGE
+                    4. :  EXIT CATEGORY
+                    """);
+            System.out.print("ENTER CHOICE : ");
+            String category=sc.next();
+
+            switch (category){
+                case "1" :
+                    String filePath="src/Question";
+                    System.out.println("----WELCOME TO JAVA QUIZ----");
+                    loadQuestion(sc , javaQuestionList, filePath);
+
+                    break;
+
+
+                case "2" :
+
+                     filePath="src/sQuestion";
+                    System.out.println("----WELCOME TO SCIENCE QUIZ----");
+
+                     loadQuestion(sc ,scienceQuestionList,  filePath);
+
+                    break;
+
+                case "3" :
+                      filePath="src/gkQuestion";
+                    System.out.println("----WELCOME TO  GENERAL KNOWLEDGE QUIZ----");
+
+                      loadQuestion(sc , gkQuestionList, filePath);
+
+                    break;
+
+                case "4" :
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("-----INVALID OPTION SELECTED");
+            }
+
+
+        }
+    }
+
+
+
+
+
+    public static void loadQuestion( Scanner sc  ,ArrayList<String[]> questionList ,String filePath) {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("src/Question"));
+            BufferedReader br = new BufferedReader(new FileReader( filePath));
 
             String line;
            while((line = br. readLine()) != null) {
-               String[] allQInPart = line.split("[//#]");
-                allQInPart =line.split("[//|]");
 
-//               String[] parts =line.split("[//|]");
+               String[] parts =line.split("[//|]");
                if (line.length() < 6) {
                    continue;
                }
-               javaList.add(new String[]{allQInPart[0]}) ;
-               scienceList.add(new String[] {allQInPart[1]}) ;
-                 gkList.add(new String[ ] {allQInPart[2]}) ;
 
-//                 allQuestionList.add(javaList);
-//               allQuestionList.add( scienceList);
-//               allQuestionList.add( gkList);
+                questionList.add(parts);
+
+
            }
             br.close();
         } catch (   IOException e) {
             System.out.println(e);
         }
 
+
+         utils.outputHandler.printQuestion(sc,  questionList);
+
     }
 
 
-    public static void AllQuestion(Scanner sc ,String category, HashMap<String, Questions> quesMap) {
-        switch (category){
-            case "1":
 
-                System.out.println("2222222222222222");
 
-                System.out.println("----WELCOME TO JAVA QUIZ----");
-                System.out.println("3333333333333333333");
-                utils.outputHandler.printQuestion(sc, javaList);
-                 break;
-            case "2":
-                System.out.println("----WELCOME TO SCIENCE QUIZ----");
-                utils.outputHandler.printQuestion(sc, scienceList);
-                break;
-            case "3":
-                System.out.println("----WELCOME TO GK QUIZ----");
-                utils.outputHandler.printQuestion(sc,  gkList);
+    public static void addQuestionInTxtFile(String data , String   filename){
+        try(BufferedWriter  br=new BufferedWriter(new FileWriter( filename , true))){
+                 br.write(data);
+                 br.newLine();
+                 br.close();
 
-                break;
-            default:
-                System.out.println("---INVALID OPTION SELECTED----");
+            System.out.println("DATA ADDED SUCCESSFULLY");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
+
+    public static void  chooseFile(Scanner sc){
+        System.out.println("""
+                ENTER :
+                1. ADD JAVA QUESTION
+                2. ADD  SCIENCE QUESTION
+                3. ADD  GK QUESTION 
+                4. EXIT QUESTION ADDING
+         """);
+
+        String choice=sc.next();
+        switch (choice){
+            case "1":
+                System.out.print("ENTER DATA TO ADD : ");
+                String data=sc.next();
+                String path="src/Question";
+                addQuestionInTxtFile(  data ,  path);
+            break;
+
+            case "2":
+                System.out.print("ENTER DATA TO ADD : ");
+                  data=sc.next();
+                  path="src/sQuestion";
+                addQuestionInTxtFile( data ,  path);
+                break;
+
+            case "3":
+                System.out.print("ENTER DATA TO ADD : ");
+                  data=sc.next();
+                  path="src/gkQuestion";
+                addQuestionInTxtFile(data ,  path);
+                break;
+
+            case "4":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("INVALID OPTION SELECTED");
+        }
+
+    }
+
+
+
+
+
+
+
+
 
 }
 
